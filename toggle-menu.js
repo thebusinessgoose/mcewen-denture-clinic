c743ac4e73a0e67ab4341d5c4e17fa6bb5c1b53b
Brian Green <b.agreen92@gmail.com>
2020-09-28 19:16:50 -0700

    refreshed the layout

---
+++ toggle-menu.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
